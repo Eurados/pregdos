9e75e36733dcf6302721dd523e3db0430d3f7a90
Niels Bassler <bassler@clin.au.dk>
2026-03-26 19:20:19 +0100

    make slurm work by compiling without systemd support

--- docker/slurm/entrypoint.sh
+++ docker/slurm/entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-sudo sed -i "s/__NPROC__/$(nproc)/g" /etc/slurm/slurm.conf
+# Reserve 1 CPU and 10% of RAM for the OS
+NCPUS=$(( $(nproc) - 1 ))
+[ "$NCPUS" -lt 1 ] && NCPUS=1
+TOTAL_MEM_MB=$(awk '/MemTotal/ {print int($2/1024)}' /proc/meminfo)
+SLURM_MEM_MB=$(( TOTAL_MEM_MB * 90 / 100 ))
 
-sudo service munge start
-sudo service slurmctld start
-sudo service slurmd start
-sudo service ssh start
+sed -i "s/__NPROC__/$NCPUS/g" /etc/slurm/slurm.conf
+sed -i "s/__REALMEM__/$SLURM_MEM_MB/g" /etc/slurm/slurm.conf
 
-exec /bin/bash
+service munge start
+slurmctld
+runuser -u slurm -- slurmd
+service ssh start
 
-# tail -f /dev/null
+exec "$@"
